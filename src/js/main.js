75c3cc5f91f8fbf2ee75b8cd26138398a8012134
Hanna Lindkvist <hali2507@student.miun.se>
2026-05-22 09:54:03 +0200

    styling mobile navigation

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  //Eventlyssnare för hamburgermeny
+  document.querySelector(".container-navbar").addEventListener("click", toggleMenu);
+});
+
+//togglefunktion för hamburgermeny
+function toggleMenu() {
+  const mainNavEl = document.querySelector(".main-nav");
+  const hamMenuEl = document.querySelector("#hamburger-menu");
+
+  if (mainNavEl.style.display === "block") {
+    mainNavEl.style.display = "none";
+    mainNavEl.style.height = "0%";
+  } else {
+    mainNavEl.style.display = "block";
+    mainNavEl.style.height = "100%";
+  }
+}

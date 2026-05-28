08eeb172da92dc24d9b377a8413540646d160e75
Hanna Lindkvist <hali2507@student.miun.se>
2026-05-28 16:02:22 +0200

    lägger till meddelande för kontaktformulär skickat

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,6 +1,22 @@
 "use strict";
 
 document.querySelector("#button-contact-form").addEventListener("click", toggleContactForm);
+const buttonSubmitContact = document.querySelector("#button-submit-contact");
+if (buttonSubmitContact) {
+    buttonSubmitContact.addEventListener("click", submitContactForm);
+}
+
+function submitContactForm() {
+    const messageEl = document.querySelector("#contact-message");
+    const contactFormDivEl = document.querySelector("#contact-form");
+
+    messageEl.innerHTML = ""; // Rensa tidigare meddelanden
+
+    messageEl.innerHTML = "<p>Tack för ditt meddelande! Vi återkommer till dig inom kort.</p>";
+
+    contactFormDivEl.style.display = "none";
+
+}
 
 //togglefunktion för kontaktformulär
 function toggleContactForm() {
